1187aeb5841e53cb04ae9f8a83581dfaab73d9aa
msgithub-hub <2095358314@qq.com>
2021-08-16 17:34:25 +0800

    8.16

--- polynkMob/js/head.js
+++ polynkMob/js/head.js
@@ -3,17 +3,25 @@ layui.use( 'jquery', () => {
     let $=layui.jquery;
     // let layer=layui.layer;
     $(function () {
+        if (returnCitySN) {
+            // var privence = returnCitySN['cname'].substr(0, 3);
+            console.log(returnCitySN);
+            $(".logoWord").empty()
+            $(".logoWord").prepend(`<i class="layui-icon layui-icon-location">${returnCitySN.cname}</i>`)
+        } else {
+            $(".logoWord").text('welcome to Polynk!')
+        }
         // 媒体查询
         $('.head-top').on('click','.menu', function () {
             $('.content-nav .navUl').slideToggle();
         });
         let index = window.location.hash.substring(1);
         $('.content-nav>.navUl>.li_son1').eq(index).addClass("li_none").siblings(".navUl .li_son1").removeClass("li_none");
-
         // $('.content-nav .navUl .li_son1').each(function(){
-        //     if($($(this))[0].window.open()==String(window.location))
+        //     if($($(this))[0].window.open('','_self')==String(window.location))
         //         $(this).addClass('li_none').siblings('.li_none').removeClass('li_none');
         // });
+
 
         $('.head-top .logo').on('click',function () {
             // $(this).addClass('flip');
